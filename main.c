6728c25c5d7ab863f310431a1cfad66e590154eb
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:47:16 +0700

    Pengisian Niali i=5, 27 Desember 2021

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Penulis : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Mengisi variable i yang bertype int dengan nilai 5 dan menuliskannya */
+
+#include <stdio.h>
+int main() {
+    /* KAMUS */
+    int i = 9; /* deklarasi dan inisialisasi nilai variabel i dengan 5 */
+    
+    /* ALGORITMA */
+    printf ("Ini nilai i = %d \n", i); /*untuk mengamati dampak inisialisasi */
+    
+    return 0;
+}
